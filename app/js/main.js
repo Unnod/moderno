289cf6a72bf2e76cf3a540cfd521163bb9ee3857
Innod <fomichev.roman@yandex.ru>
2019-11-29 17:59:29 +0300

    added slider

--- app/js/main.js
+++ app/js/main.js
@@ -1,4 +1,21 @@
-$(function(){
+$(function () {
+
+
+
+    $(".stars").rateYo({
+        rating: 5,
+        starWidth: "12px",
+        readOnly: true
+
+    });
+
+    $('.products-slider__inner').slick({
+        slidesToShow: 4,
+        slidesToScroll: 4,
+        arrows: false,
+        dots: true
+    });
+
 
     var mixer = mixitup('.products__inner-box');
 
