962696ac502e386d53ef92094e191894e7d730b6
Innod <fomichev.roman@yandex.ru>
2019-11-30 18:30:49 +0300

    added new-items

--- app/js/main.js
+++ app/js/main.js
@@ -16,6 +16,15 @@ $(function () {
         dots: true
     });
 
+    $(".js-range-slider").ionRangeSlider({
+        type: "double",
+        min: 0,
+        max: 1000,
+        from: 0,
+        to: 600,
+        prefix: "$"
+    });
+
 
     var mixer = mixitup('.products__inner-box');
 
